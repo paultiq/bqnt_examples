422c91063a106d59fbfc6f7cf8442f135411f07f
paul@iqmo.com <paul@iqmo.com>
2024-04-29 19:08:17 -0400

    Add pre-commits / nbstrip

--- bql_stub/bql.py
+++ bql_stub/bql.py
@@ -1,43 +1,24 @@
 """This is a simple stubbed BQL that returns pre-defined results for pre-defined queries. 
 Not a general simulator, just for blogging purposes.
 """
-import json
-import pandas as pd
-import hashlib
-from pathlib import Path
 
+import bql_faker
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
-MODULE_DIR = Path(__file__).parent
-QUERY_CACHE_FILE = MODULE_DIR / "queries.json"
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
 
 def combined_df(response: dict):
-    return pd.read_json(response)
+    return response
 
-def _hash(query: str):
-    return hashlib.md5(query.encode()).hexdigest()
 
-class Service():
-    query_cache = {}
+class Service:
 
     def __init__(self):
-        with open(QUERY_CACHE_FILE, 'r') as f:
-            self.query_cache = json.load(f)
+        pass
 
     def execute(self, query: str):
-
-        if _hash(query) not in self.query_cache:
-            print(f"Unexpected query {_hash(query)=}")
+        if query not in bql_faker.QUERY_RESULTS:
+            raise ValueError("Query not defined")
         else:
-            return self.query_cache[_hash(query)]
-
-def _save(query, df):
-    """Saves to the query """
-    with open(QUERY_CACHE_FILE, 'r') as f:
-        query_cache = json.load(f)
-
-    query_cache[_hash(query)] = df.to_json(date_format='iso')
-
-    with open(QUERY_CACHE_FILE, 'w') as f:
-        json.dump(query_cache, f)
+            return bql_faker.QUERY_RESULTS[query]
---
+++ bql_stub/bql_faker.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+
+
+corr_query = """
+    get(sales_rev_turn)
+    for(members('SPX Index'))
+    with(fpo=range(-9Q, 0Q), fpt=Q, fill=prev)
+    preferences(addcols=all)
+"""
+
+basics_query = """
+    get(
+      px_last
+    ) for(
+      'IBM US Equity'
+    ) with(
+      dates=range(-29d, 0d),
+      fill=prev
+    )
+"""
+
+QUERY_RESULTS = {}
+
+
+def basics():
+    # Data for Basics
+
+    periods = 30
+
+    dates = pd.date_range(
+        start=pd.Timestamp.now() - pd.DateOffset(years=1), periods=periods
+    ).normalize()
+
+    basics_response = pd.DataFrame(
+        {"ID": "IBM US Equity", "DATE": dates, "px_last": np.random.rand(periods) * 10}
+    )
+
+    return basics_response
+
+
+def corr():
+    start_year = 2020
+    num_periods = 10
+    periods = [f"{start_year + n//4} Q{n%4+1}" for n in range(num_periods, 0, -1)]
+    period_offset = [
+        f"{n - num_periods if n != num_periods else 0}Q"
+        for n in range(num_periods, 0, -1)
+    ]
+    companies = [f"CO{n+1} FAKE" for n in range(0, 500)]
+
+    dfs: list[pd.DataFrame] = []
+    for co in companies:
+        co_df = pd.DataFrame(
+            {
+                "ID": co,
+                "PERIOD": periods,
+                "PERIOD_OFFSET": period_offset,
+                "sales_rev_turn": np.random.rand(num_periods) * 1000,
+            }
+        )
+        dfs.append(co_df)
+
+    corr_response: pd.DataFrame = pd.concat(dfs).reset_index(drop=True).set_index("ID")  # type: ignore
+    return corr_response
+
+
+QUERY_RESULTS[basics_query] = basics()
+QUERY_RESULTS[corr_query] = corr()
