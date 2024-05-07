523429e2dffc21a1636f15dfc74e7dcb5ca23f61
paul@iqmo.com <paul@iqmo.com>
2024-05-06 20:54:05 -0400

    Updating for test cases

--- test/test_nb.py
+++ test/test_nb.py
@@ -1,18 +1,31 @@
-import os
+from pathlib import Path
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 import pytest
+import dotenv
+import os
 
-skip_notebooks = ["bqnt_profiling.ipynb", "bqnt_iql_magics.ipynb"]
+dotenv.load_dotenv(dotenv_path="test/.default_env", override=False)
+
+NOTEBOOK_DIR = Path(os.environ["NOTEBOOK_DIR"])
+SEARCH = os.environ["SEARCH"]
+SKIP_NOTEBOOKS = os.environ["SKIP_NOTEBOOKS"].split(" ")
 
 
 @pytest.mark.parametrize(
     "notebook",
-    [f for f in os.listdir("..") if f.endswith(".ipynb") and f not in skip_notebooks],
+    [
+        f
+        for f in NOTEBOOK_DIR.glob(SEARCH)
+        if f.name not in SKIP_NOTEBOOKS
+        and ".ipynb_checkpoints" not in str(f.absolute())
+    ],
 )
 def test_notebook(notebook):
-
-    with open(os.path.join("..", notebook)) as f:
+    with open(notebook) as f:
         nb = nbformat.read(f, as_version=4)
     ep = ExecutePreprocessor(timeout=600)
     ep.preprocess(nb)
+
+
+# pytest -n 10
